ac7ed6e5657c1535cac3752b60c49573ecafa855
Muhammad Rafael Ghifari <business.rafaelghifari@gmail.com>
2025-07-28 17:25:10 +0700

    Add project files.

---
+++ My Project/AssemblyInfo.vb
@@ -0,0 +1,32 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("SysCollectorPro")>
+<Assembly: AssemblyDescription("Enterprise-grade system audit and IT asset inventory tool for Windows environments. Provides detailed hardware, OS, network, and security configuration reporting from Windows 7 through Windows 11.")>
+<Assembly: AssemblyCompany("Flontive")>
+<Assembly: AssemblyProduct("SysCollectorPro")>
+<Assembly: AssemblyCopyright("© 2025 Flontive")>
+<Assembly: AssemblyTrademark("SysCollectorPro")>
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("d90e08e9-1bfa-4277-86e2-f4419a9051d6")>
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version
+'      Build Number
+'      Revision
+'
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
